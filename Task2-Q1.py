6484cf5a83a6277739ee4714af2152afd6e2eff5
zero.nabeel@gmail.com <57975428+zero52100@users.noreply.github.com>
2024-02-23 18:11:19 +0530

    .

--- Task2-Q1.py
+++ Task2-Q1.py
@@ -9,14 +9,18 @@ class Cypher():
                 convert_string += str((int(char)+1)% 10)
             elif char.isalpha():
                  offset = ord('A' if char.isupper() else 'a')
-                 convert_string+= chr((ord(char) - offset + 2) % 26 + offset)
-        else:
-            convert_string += char
+                 character= chr((ord(char) - offset + 2) % 26 + offset)
+                 if char.isupper():
+                    convert_string += character.lower()
+                 else:
+                    convert_string += character.upper()
+            else:
+                convert_string += char
         return convert_string
     def do_conversion(self):
         converted_string = self.conversion(self.string)
         print("Converted String:", converted_string)
         return converted_string
-    
-c1=Cypher("ABcD1293Z")
+user_input=input("Enter the Cypher text :")
+c1=Cypher(user_input)
 c1.do_conversion()
